dc99b1c9208927c209dc65e9661726708622dbcd
Richard Scheid <richard.s@involves.com.br>
2019-09-21 11:14:38 -0300

    installing axios

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,8 +1,28 @@
+const axios = require('axios');
+const Dev = require('../models/Dev');
+
 module.exports = {
-    store(req, res) {
+    async store(req, res) {
 
         const { username } = req.body;
 
-        return res.json({ ok: true });
+        const userExists = await Dev.findOne({ user:username });
+
+        if (userExists) {
+            return res.json(userExists);
+        }
+
+        const response = await axios.get(`https://api.github.com/users/${username}`);
+
+        const { name, bio, avatar_url: avatar } = response.data;
+
+        const dev = await Dev.create({
+            name: name,
+            user: username,
+            bio: bio,
+            avatar: avatar,
+        });
+
+        return res.json(dev);
     }
-}
+};
