aae6b74d4a2dccd67db5e20cd0fff181268ac99a
Richard Scheid <richard.s@involves.com.br>
2019-10-01 20:54:56 -0300

    creating frontend project

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express'); // import
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes');
 const server = express(); // creating server
 
@@ -7,6 +9,7 @@ mongoose.connect('mongodb+srv://onmistack:onmistack@cluster0-mzlt4.mongodb.net/o
     useNewUrlParser: true
 });
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
