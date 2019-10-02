0ca21b04fdda4f6ee39fc7a23be817772cdf5fa9
Richard Scheid <richard.s@involves.com.br>
2019-10-01 22:25:16 -0300

    implementation of main

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
 
-export default function Main() {
+export default function Main({ match }) {
     return (
-        <div></div>
+        <div className="main-container">
+            <img src={logo} alt="Tindev" />
+            <ul>
+                <li>
+                    <img src="https://avatars0.githubusercontent.com/u/17483394?v=4" alt="" />
+                    <footer>
+                        <strong>Richard Scheid</strong>
+                        <p>Software Engineer</p>
+                    </footer>
+                </li>
+            </ul>
+
+            <div className="buttons">
+                <button type="button">
+                    <img src={dislike} alt="Dislike" />
+                </button>
+
+                <button type="button">
+                    <img src={like} alt="Like" />
+                </button>            
+            </div>
+        </div>
     );
 }
